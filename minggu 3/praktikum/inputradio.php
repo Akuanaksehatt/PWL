466d806bf4c3f696d8672298cfe77abaedd8c15f
Akuanaksehatt <95815495+Akuanaksehatt@users.noreply.github.com>
2022-03-29 22:55:02 +0700

    Update inputradio.php

--- minggu 3/praktikum/inputradio.php
+++ minggu 3/praktikum/inputradio.php
@@ -6,7 +6,7 @@
 		<input type="radio" name="jurusan" value="TI" checked> Teknik Informatika<br>
 		<input type="radio" name="jurusan" value="SI"> Sistem Informasi<br>
 		<input type="radio" name="jurusan" value="SK"> Sistem Komputer<br>
-		<input type="radio" name="jurusan" value="SK"> Keamanan Siber<br>
+		<input type="radio" name="jurusan" value="KS"> Keamanan Siber<br>
 		<input type="radio" name="jurusan" value="KA"> Komputerisasi Akuntansi<br>
 		<input type="submit" name="Pilih" value="Pilih">
 		</FORM>
