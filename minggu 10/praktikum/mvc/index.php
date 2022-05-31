4776d7524abaf2be91dfb63d2c3b99a439e9b7a8
Akuanaksehatt <95815495+Akuanaksehatt@users.noreply.github.com>
2022-05-31 13:46:51 +0700

    Add files via upload

---
+++ minggu 10/praktikum/mvc/index.php
@@ -0,0 +1,8 @@
+<?php
+// All interaction goes through the index and isforwarded
+// directly to the controller
+
+	include_once("controller/Controller.php");
+	$controller = new Controller();
+	$controller->invoke();
+?>
